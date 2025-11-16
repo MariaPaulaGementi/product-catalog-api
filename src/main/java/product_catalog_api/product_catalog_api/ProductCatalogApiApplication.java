34f96df8084a3ddb2f79c2b44e48ca5cbb604f98
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2025-11-16 10:37:09 -0300

    First commit the project

---
+++ src/main/java/product_catalog_api/product_catalog_api/ProductCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package product_catalog_api.product_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApiApplication.class, args);
+	}
+
+}
